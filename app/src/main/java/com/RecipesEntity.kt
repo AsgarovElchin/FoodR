98c80df19cc111bd6a6f98c7c0b2c80ae083b9d1
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-07-23 21:41:39 +0400

    entites and dao done

---
+++ app/src/main/java/com/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.models.FoodRecipe
+
+@Entity(tableName = "recipes_table")
+class RecipesEntity (
+    var foodRecipe: FoodRecipe
+        )
+    {
+        @PrimaryKey(autoGenerate = true)
+        var id : Int = 0
+    }
