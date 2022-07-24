1791e4779ac7ae983ec256c15381f35afe620b47
ElchinCyrus <71493515+ElchinCyrus@users.noreply.github.com>
2022-07-24 15:02:36 +0400

    DatabaseModule class done

---
+++ app/src/main/java/com/di/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.di
+
+import android.content.Context
+import androidx.room.Room
+import com.database.RecipesDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context, RecipesDatabase::class.java, "recipes_database").build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: RecipesDatabase)=database.recipesDao()
+}
